f740a8b3625d3b9e5e2729fc907bd00227b91025
Cviki <cvikicivik@gmail.com>
2026-03-31 00:35:06 +0200

    complete the exercises 1.3 and 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,19 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const parts = [part1, part2, part3]
-  const allexercises = [exercises1, exercises2, exercises3]
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header course = {course} />
-      <Content part = {parts} exercise = {allexercises} />
-      <Total total = {allexercises} />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
@@ -29,9 +35,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part[0]} exercise = {props.exercise[0]} />
-      <Part part={props.part[1]} exercise = {props.exercise[1]} />
-      <Part part={props.part[2]} exercise = {props.exercise[2]} />
+      <Part part={props.parts[0].name} exercise = {props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercise = {props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercise = {props.parts[2].exercises} />
     </div>
   )
 }
@@ -47,14 +53,9 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-  let sum = 0
-  for (let i = 0; i < props.total.length; i++) {
-  sum += props.total[i]
-}
-  
   return(
     <div>
-      <p>Number of exercises {sum}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </div>
   )
 }
